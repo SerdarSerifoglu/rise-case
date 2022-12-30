8c0da0bd097fb81e05f4b23a66350f5e9cd68d81
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-12-30 21:07:17 +0300

    priority names changed

--- pages/api/priorities.js
+++ pages/api/priorities.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json([
-    { id: 1, name: "Acil", color: "red" },
-    { id: 2, name: "Önemli", color: "purple" },
+    { id: 1, name: "Urgent", color: "red" },
+    { id: 2, name: "Important", color: "purple" },
     { id: 3, name: "Normal", color: "blue" },
   ]);
 }
