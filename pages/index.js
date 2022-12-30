1d8e7580e78dcec5b7289f3d65d9c6a22faec3dc
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-12-30 14:35:28 +0300

    vercel test

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,8 @@ const Home = (props) => {
 
 export async function getServerSideProps() {
   // Fetch data from an API or database
-  const res = await fetch("http://localhost:3000/api/priorities");
+  // const res = await fetch("http://localhost:3000/api/priorities");
+  const res = await fetch("https://rise-case.vercel.app/api/priorities");
   const priorities = await res.json();
 
   // Pass data to the page via props
