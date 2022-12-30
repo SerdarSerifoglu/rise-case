ecb3e4d90fb4bd87203c62629d99911bb1f228fd
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-12-30 22:03:18 +0300

    ErrorBoundary test

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { Provider } from "react-redux";
+import ErrorBoundary from "../components/ErrorBoundary.js";
 import { store } from "../redux/store.js";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ErrorBoundary>
   );
 }
