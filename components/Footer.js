d42f5aa2cbf8c11a1a8159740ebed3c98d60e437
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-12-30 21:47:28 +0300

    top and footer added

---
+++ components/Footer.js
@@ -0,0 +1,60 @@
+import { Grid } from "@mui/material";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <>
+      <hr />
+      <GridWrapper container>
+        <GithubRepo
+          item
+          md={6}
+          display="flex"
+          alignItems="flex-start"
+          justifyContent="flex-start"
+        >
+          <LinkStyle
+            target="_blank"
+            href="https://github.com/SerdarSerifoglu/rise-case"
+          >
+            Github Repository
+          </LinkStyle>
+        </GithubRepo>
+        <PersonalInfo
+          item
+          md={6}
+          display="flex"
+          alignItems="flex-end"
+          justifyContent="flex-end"
+        >
+          <LinkStyle
+            target="_blank"
+            href="https://www.linkedin.com/in/omer-serdar-serifoglu/"
+          >
+            2023 - Ömer Serdar Şerifoğlu
+          </LinkStyle>
+        </PersonalInfo>
+      </GridWrapper>
+    </>
+  );
+};
+
+const GridWrapper = styled(Grid)`
+  height: 64px;
+  line-height: 64px;
+  background-color: #dddddd;
+`;
+
+const LinkStyle = styled.a`
+  text-decoration: none;
+  color: black;
+  font-weight: 700;
+`;
+const GithubRepo = styled(Grid)`
+  padding-left: 10px;
+`;
+const PersonalInfo = styled(Grid)`
+  padding-right: 10px;
+`;
+
+export default Footer;
---
+++ components/Top.js
@@ -0,0 +1,24 @@
+import { Grid } from "@mui/material";
+import styled from "styled-components";
+
+const Top = () => {
+  return (
+    <>
+      <Grid
+        container
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Header>Personal Job Tracking App</Header>
+      </Grid>
+      <hr />
+    </>
+  );
+};
+
+const Header = styled.div`
+  font-size: 48px;
+`;
+
+export default Top;
